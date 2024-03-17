fb0912ed5e4aba9a9fcd33abd50b412d06b8e21f
Lucas Barbero <lucas.vulcano.lubricacion@gmail.com>
2024-03-17 09:26:53 -0300

    Elimino vitepress y agrego un proyecto nuevo para la documentacion

---
+++ documentation/src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <Typography variant="h1">The Tailwind CSS Components Library For Coding 10x Faster</Typography>
+</template>
+
+<script lang="ts" setup>
+import { Typography } from 'material-tailwind-vue'
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/material-tailwind-vue/**/*.js"
   ],
   theme: {
     extend: {},
